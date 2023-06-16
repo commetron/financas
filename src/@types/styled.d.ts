c14e8dd30aec3c44bcd440f0b9710f459736d95c
commetron <softechstar.pdf@outlook.com>
2023-06-16 20:06:21 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+
+import { defaultTheme } from './../styles/themes/default';
+import 'styled-components';
+
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+
+  export interface defaultTheme extends ThemeType {}
+}
